3da67dc1fb3341f77e6be62da0a6b1ebd9b85078
yaboiedchoi <ec2244@rit.edu>
2024-04-19 20:46:01 -0400

    player movement added

---
+++ ScoreSpaceJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
