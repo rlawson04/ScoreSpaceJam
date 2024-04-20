2fd495f0c8078bddc865e89c863bd068233d3839
rlawson04 <rbl2991@rit.edu>
2024-04-19 22:41:49 -0400

    Amazing doors

--- ScoreSpaceJam/Assets/Scripts/Room.cs
+++ ScoreSpaceJam/Assets/Scripts/Room.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     private bool hasEnemies;
+    [SerializeField] private GameObject door;
 
     // Start is called before the first frame update
     void Start()
     {
         hasEnemies = true;
+        door.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Player.instance.transform.position.y > transform.position.y && hasEnemies)
+        {
+            door.SetActive(true);
+        }
     }
 }
