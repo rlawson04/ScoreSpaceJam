e8a69d2daeba0b82934c8d3f299583e1c6cbc9d3
rlawson04 <rbl2991@rit.edu>
2024-04-19 22:25:34 -0400

    Tile Mapping

---
+++ ScoreSpaceJam/Assets/Scripts/Room.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    private bool hasEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasEnemies = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
