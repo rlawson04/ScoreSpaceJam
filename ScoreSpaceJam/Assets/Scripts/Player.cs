fb97911fa58d912881f528c8ac9b873861b3e0bb
yaboiedchoi <ec2244@rit.edu>
2024-04-19 22:03:15 -0400

    added new player class

--- ScoreSpaceJam/Assets/Scripts/Player.cs
+++ ScoreSpaceJam/Assets/Scripts/Player.cs
@@ -2,10 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    public static Player instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Feilds
     [SerializeField]
     private float moveSpeed = 5f;
+
+    [SerializeField]
+    private uint health = 10;
+
+    // Properties
+    public uint Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
 
     // Start is called before the first frame update
     void Start()
