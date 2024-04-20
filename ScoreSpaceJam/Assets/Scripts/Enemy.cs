b0b8314e50c3d67b04b7237015b7e4d77e9a9e55
rlawson04 <rbl2991@rit.edu>
2024-04-20 02:44:45 -0400

    Enemy made with basic follow

--- ScoreSpaceJam/Assets/Scripts/Enemy.cs
+++ ScoreSpaceJam/Assets/Scripts/Enemy.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    uint health;
+    float speed;
+    uint damage;
+    float range;
+
     // Start is called before the first frame update
     void Start()
     {
         EnemyManager.instance.MakeEnemy();
+        health = 10;
+        speed = 1;
+        damage = 10;
+        range = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector2.Distance(Player.instance.transform.position, transform.position);
+
+        if (distance < range)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.instance.transform.position, speed * Time.deltaTime);
+        }
+
+        if(health <= 0)
+        {
+            EnemyManager.instance.RemoveEnemy(this);
+        }
     }
 }
